71a1793cfdfc72602123c33c6e846698949b79b7
Kate Miholenko <katemixa92@gmail.com>
2022-06-28 00:14:30 +0300

    refactor: before and after for class, changed access modifiers

--- src/test/java/TutorialsPointTest.java
+++ src/test/java/TutorialsPointTest.java
@@ -1,16 +1,13 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TutorialsPointTest{
+public class TutorialsPointTest {
 
-    public ChromeDriver webDriver;
+    public static ChromeDriver webDriver;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         System.setProperty("webserver.chrome.driver", "C:\\Programs\\chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
@@ -25,15 +22,15 @@ public class TutorialsPointTest{
     }
 
     @Test
-    public void verifyHeading(){
+    public void verifyHeading() {
         System.out.println("In Verify Heading Text Above Search Test Case");
         String expectedHeading = "Simply Easy Learning at your fingertips";
         String actualHeading = webDriver.findElement(By.xpath("/html/body/main/section[1]/div/div/div[1]/h1")).getText();
         Assert.assertEquals(expectedHeading, actualHeading);
     }
 
-    @After
-    public void closeWebDriver() throws InterruptedException {
+    @AfterClass
+    public static void closeWebDriver() throws InterruptedException {
         Thread.sleep(5000);
         webDriver.quit();
     }
